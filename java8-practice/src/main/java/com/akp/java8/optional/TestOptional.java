ea9fb224714b59da0775b1cf5ec03d2965979fdf
akp <parida.akshaya@gmail.com>
2018-07-14 18:39:30 -0400

    Updated

--- java8-practice/src/main/java/com/akp/java8/optional/TestOptional.java
+++ java8-practice/src/main/java/com/akp/java8/optional/TestOptional.java
@@ -213,3 +213,56 @@ public class TestOptional {
 	}
 
 }
+
+class Person {
+	private String name;
+	private int age;
+	private String password;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public Optional<String> getName() {
+		return Optional.ofNullable(name);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Optional<Integer> getAge() {
+		return Optional.ofNullable(age);
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Optional<String> getPassword() {
+		return Optional.ofNullable(password);
+	}
+
+}
+
+class Modem {
+	private Double price;
+
+	public Modem(Double price) {
+		this.price = price;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+}
