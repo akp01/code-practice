ee404cb98f98930d9cdee528a32e097700a91982
akp <parida.akshaya@gmail.com>
2018-07-14 18:37:09 -0400

    Updated

--- java8-practice/src/main/java/com/akp/java8/optional/OptionalBasicExample.java
+++ java8-practice/src/main/java/com/akp/java8/optional/OptionalBasicExample.java
@@ -13,10 +13,9 @@ public class OptionalBasicExample {
 		System.out.println("Non-Empty Optional:" + gender);
 		System.out.println("Non-Empty Optional: Gender value : " + gender.get());
 		System.out.println("Empty Optional: " + Optional.empty());
-
 		System.out.println("ofNullable on Non-Empty Optional: " + Optional.ofNullable(answer1));
+		
 		System.out.println("ofNullable on Empty Optional: " + Optional.ofNullable(answer2));
-
 		// java.lang.NullPointerException
 		System.out.println("ofNullable on Non-Empty Optional: " + Optional.of(answer2));
 
