a598877d80a3fcd8594817400d0a2ba218e19a9f
akp <parida.akshaya@gmail.com>
2019-11-04 18:24:53 -0500

    Updated

--- data-structure/src/main/java/com/akp/ds/string/palindrome/PalindromeTest2.java
+++ data-structure/src/main/java/com/akp/ds/string/palindrome/PalindromeTest2.java
@@ -4,39 +4,38 @@ import java.util.Scanner;
 
 public class PalindromeTest2 {
 	public static void main(String args[]) {
-	       
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Please enter a String");
-        String input = reader.nextLine();
-        
-        System.out.printf("Is %s a palindrome? : %b %n", input, isPalindrome(input));
-        
-        
-        System.out.println("Please enter another String");
-        input = reader.nextLine();
-        
-        System.out.printf("Is %s a palindrome? : %b %n", input, isPalindrome(input));
-        
-        reader.close();
-        
 
-    }
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Please enter a String");
+		String input = reader.nextLine();
 
-    public static boolean isPalindrome(String input) {
-        if (input == null || input.isEmpty()) {
-            return true;
-        }
+		System.out.printf("Is %s a palindrome? : %b %n", input, isPalindrome(input));
 
-        char[] array = input.toCharArray();
-        StringBuilder sb = new StringBuilder(input.length());
-        for (int i = input.length() - 1; i >= 0; i--) {
-            sb.append(array[i]);
-        }
+		System.out.println("Please enter another String");
+		input = reader.nextLine();
 
-        String reverseOfString = sb.toString();
+		System.out.printf("Is %s a palindrome? : %b %n", input, isPalindrome(input));
 
-        return input.equals(reverseOfString);
-    }
+		reader.close();
 
+	}
+
+	public static boolean isPalindrome(String input) {
+		if (input == null || input.isEmpty()) {
+			return true;
+		}
+
+		char[] array = input.toCharArray();
+		StringBuilder sb = new StringBuilder(input.length());
+		
+		for (int i = input.length() - 1; i >= 0; i--) {
+			sb.append(array[i]);
+		}
+
+		String reverseOfString = sb.toString();
+
+		return input.equals(reverseOfString);
+	}
 
 }
