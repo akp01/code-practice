6c9626ee8b92460c6f4dbd07ef10e58033055ba5
akp <parida.akshaya@gmail.com>
2019-12-01 21:59:30 -0500

    Added

--- data-structure/src/main/java/com/akp/ds/linkedlist/DeleteLinkedListNodeAtAGivenPosition.java
+++ data-structure/src/main/java/com/akp/ds/linkedlist/DeleteLinkedListNodeAtAGivenPosition.java
@@ -1,46 +1,91 @@
 package com.akp.ds.linkedlist;
 
 public class DeleteLinkedListNodeAtAGivenPosition {
-	Node head;
 
+	Node head; // head of list
+
+	/* Linked list Node */
 	class Node {
-		private String data;
-		private Node next;
+		int data;
+		Node next;
 
-		public Node(String data) {
-			this.data = data;
-			this.next = null;
-
+		Node(int d) {
+			data = d;
+			next = null;
 		}
 	}
 
-	private void addElement(Node newNode) {
-		newNode.next = head;
-		head = newNode;
+	/* Inserts a new Node at front of the list. */
+	public void push(int value) {
+		Node new_node = new Node(value);
+
+		new_node.next = head;
+		head = new_node;
 	}
 
-	private void displayLinkedlIst() {
-		Node node = head;
-		while (node != null) {
-			System.out.print(node.data + " ");
-			node = node.next;
+	/*
+	 * Given a reference (pointer to pointer) to the head of a list and a position,
+	 * deletes the node at the given position.
+	 */
+	void deleteNode(int position) {
+		// If linked list is empty
+		if (head == null)
+			return;
 
+		// Store head node
+		Node temp = head;
+
+		// If head needs to be removed
+		if (position == 0) {
+			head = temp.next; // Change head
+			return;
+		}
+
+		// Find previous node of the node to be deleted
+		for (int i = 0; temp != null && i < position - 1; i++)
+			temp = temp.next;
+
+		// If position is more than number of ndoes
+		if (temp == null || temp.next == null)
+			return;
+
+		// Node temp->next is the node to be deleted
+		// Store pointer to the next of node to be deleted
+		Node next = temp.next.next;
+
+		temp.next = next; // Unlink the deleted node from list
+	}
+
+	/*
+	 * This function prints contents of linked list starting from the given node
+	 */
+	public void printList() {
+		Node tnode = head;
+		while (tnode != null) {
+			System.out.print(tnode.data + " ");
+			tnode = tnode.next;
 		}
 	}
 
-	private void deleteNodeAtAGivenPosition(int position) {
-
-		Node current = head;
-		Node prev = null;
-
-		while (current != null) {
-
-		}
-
-	}
-
+	/*
+	 * Drier program to test above functions. Ideally this function should be in a
+	 * separate user class. It is kept here to keep code compact
+	 */
 	public static void main(String[] args) {
 
-	}
+		DeleteLinkedListNodeAtAGivenPosition llist = new DeleteLinkedListNodeAtAGivenPosition();
+		llist.push(7);
+		llist.push(1);
+		llist.push(3);
+		llist.push(2);
+		llist.push(8);
 
+		System.out.println("\nCreated Linked list is: ");
+		llist.printList();
+
+		llist.deleteNode(4); 
+
+		System.out.println("\nLinked List after Deletion at position 4: ");
+		llist.printList();
+	}
 }
